e709658e71987c5fe4b389b83923028a7b3d9996
Administrator <i@ruok.cc>
2023-06-21 22:39:00 +0800

    部分表情

---
+++ src/main/java/cc/ruok/ja_cqhttp/cq/Face.java
@@ -0,0 +1,177 @@
+package cc.ruok.ja_cqhttp.cq;
+
+public enum Face {
+
+    AMAZED(0),      //惊讶
+    POUT(2),        //撇嘴
+    SEX(3),         //色
+    BLANKLY(4),     //发呆
+    PROUD(5),       //得意
+    SHY(6),         //害羞
+    SHUT_UP(7),     //闭嘴
+    SLEEP(8),       //睡
+    CRY(9),         //大哭
+    AWKWARD(10),    //尴尬
+    ANGER(11),      //发怒
+    NAUGHTY(12),    //调皮
+    TEETH(13),      //龇牙
+    SMILE(14),      //微笑
+    HARD(15),       //难过
+    COOL(16),       //酷
+    CRAZY(18),      //抓狂
+    VOMIT(19),      //吐
+    TITTER(20),     //偷笑
+    LOVELY(21),     //可爱
+    EYES(22),       //白眼
+    ARROGANT(23),   //傲慢
+    HUNGER(24),     //饥饿
+    SLEEPY(25),     //困
+    TERRIFIED(26),  //惊恐
+    SWEAT(27),      //流汗
+    SIMPER(28),     //憨笑
+    CAREFREE(29),   //悠闲
+    STRUGGLE(30),   //奋斗
+    CURSE(31),      //咒骂
+    QUERY(32),      //疑问
+    SH(33),         //嘘
+    SWOON(34),      //晕
+    SUFFERING(35),  //折磨
+    LOST(36),       //衰
+    SKELETON(37),   //骷髅
+    BEAT(38),       //敲打
+    BYE(39),        //再见
+    SHIVER(41),     //发抖
+    LOVE(42),       //爱情
+    JUMP(43),       //跳跳
+    PIG(46),        //猪头
+    HUG(49),        //拥抱
+    CAKE(53),       //蛋糕
+    LIGHTNING(54),  //闪电
+    BOMB(55),       //炸弹
+    KNIFE(56),      //刀
+    FOOTBALL(57),   //足球
+    SHIT(59),       //便便
+    COFFEE(60),     //咖啡
+    RICE(61),       //米饭
+    MEDICINE(62),   //药
+    FLOWER(63),     //玫瑰
+    FADE(64),       //凋谢
+    HEART(66),      //爱心
+    BROKEN_HEART(67),   //心碎
+    GIFT(69),       //礼物
+    MAIL(72),       //信
+    SUN(74),        //太阳
+    MOON(75),       //月亮
+    GOOD(76),       //赞
+    STEP(77),       //踩
+    HANDSHAKE(78),  //握手
+    YEAH(79),       //胜利
+    AIR_KISS(85),   //飞吻
+    ANNOYED(86),    //怄火
+    WATERMELON(89), //西瓜
+    RAIN(90),       //雨
+    CLOUD(91),      //云
+    COLD_SWEAT(96), //冷汗
+    WIPE(97),       //擦汗
+    NOSE_PICKING(98),   //抠鼻
+    APPLAUD(99),    //鼓掌
+    EMBARRASS(100), //出糗
+    GRIN(101),      //坏笑
+    LEFT_HUM(102),  //左哼哼
+    RIGHT_HUM(103), //右哼哼
+    YAWN(104),      //哈欠
+    DESPISE(105),   //鄙视
+    WRONGED(106),   //委屈
+    WILL_CRY(107),  //快哭了
+    CRAFTY(108),    //阴险
+    LEFT_KISS(109), //左亲亲
+    FRIGHTEN(110),  //吓
+    POOR(111),      //可怜
+    COOK_COPPER(112),   //菜刀
+    BEER(113),      //啤酒
+    BASKETBALL(114),//篮球
+    PING_PONG(115), //乒乓球
+    SHOW_LOVE(116), //示爱
+    LADYBUG(117),   //瓢虫
+    SALUTING(118),  //抱拳
+    LURE(119),      //勾引
+    FIST(120),      //拳头
+    WORST(121),     //差劲
+    LOVE_YOU(122),  //爱你
+    NO(123),        //NO
+    OK(124),        //OK
+    ROTATE(125),    //转圈
+    KOWTOW(126),    //磕头
+    LATER(127),     //回头
+    SKIPPING(128),  //跳绳
+    WAVE(129),      //挥手
+    EXCITE(130),    //激动
+    DANCE(131),     //街舞
+    OFFER_KISS(132),//献吻
+    LEFT_TAICHI(133),   //左太极
+    RIGHT_TAICHI(134),  //右太极
+    HAPPINESS(136), //双喜
+    MAROON(137),    //鞭炮
+    LANTERN(138),   //灯笼
+    WEALTH(139),    //发财
+    KARAOKE(140),   //K歌
+    SHOPPING(141),  //购物
+    _MAIL(142),     //邮件
+    CHESS_KING(143),//帅
+    CHEER(144),     //喝彩
+    CANDLE(145),    //蜡烛
+    TENDON(146),    //爆筋
+    LOLLIPOP(147),  //棒棒糖
+    FEEDER(148),    //奶瓶
+    NOODLES(149),   //面条
+    BANANA(150),    //香蕉
+    PLANE(151),     //飞机
+    CAR(152),       //汽车
+    LEFT_LOCOMOTIVE(153),   //左车头
+    TRAIN_CAR(154), //车厢
+    RIGHT_LOCOMOTIVE(154),  //右车头
+    _RAIN(156),     //雨
+    _CLOUD(157),    //云
+    MONEY(158),     //钞票
+    PANDA(159),     //熊猫
+    LIGHT(160),     //灯泡
+    PINWHEEL(161),  //风车
+    ALARM_CLOCK(162),   //闹钟
+    UMBRELLA(163),  //伞
+    BALLOON(164),   //气球
+    RING(165),      //戒指
+    SOFA(166),      //沙发
+    PAPER(167),     //卫生纸
+    _MEDICINE(168), //药
+    GUN(169),       //手枪
+    FROG(170),      //青蛙
+    TEA(171),       //茶
+    BLINK(172),     //眨眼
+    TEARS(173),     //泪奔
+    WHOEVER(174),   //无奈
+    ACT_CUTE(175),  //卖萌
+    ENTANGLED_WITH(176),    //纠结
+    BLOOD(177),     //喷血
+    FUNNY(178),     //斜眼笑
+    HUAJI(178),     //斜眼笑
+    DOGE_YELLOW(179),   //DOGE
+    SURPRISED(180), //惊喜
+    HARASS(181),    //骚扰
+    TEARS_OF_JOY(182),  //笑哭
+    STINKY_BEAUTY(183), //我最美
+    ;
+
+    private int id;
+    Face(int id) {
+        this.id = id;
+    }
+
+    public static String get(int id) {
+        return "[CQ:face,id=" + id + "]";
+    }
+
+    @Override
+    public String toString() {
+        return "[CQ:face,id=" + id + "]";
+    }
+}
